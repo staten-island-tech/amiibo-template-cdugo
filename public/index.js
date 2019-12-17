624945ff615e07d7d336d2e2e03ec20a4bde3e15
cdugo <54993517+cdugo@users.noreply.github.com>
2019-12-17 11:45:44 -0500

    h

--- public/index.js
+++ public/index.js
@@ -2,8 +2,21 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("Thami likes bagels and butter. Also he smells");
+//def paths for express
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views');
+
+
+//setup handlebars engine and views location
+
+app.set('views engine', 'hbs'); //telling express/node to use handlebars for templates
+app.set('views', viewsPath)//telling express to get templates from templates/views
+app.get("", async(req, res) => {
+  try{
+    res.render('index');
+  } catch (error) {
+    res.status(500).send('Page not found');
+  }
 });
 app.get("/thamisucks", (req, res) => {
   res.send("Thami sucks. And Yes I have to do this while you eat");
