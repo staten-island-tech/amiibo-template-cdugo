2575d7c597cdcac202dceb21f4154fd7f1b81a10
cdugo <54993517+cdugo@users.noreply.github.com>
2019-12-18 11:45:06 -0500

    j

--- public/index.js
+++ public/index.js
@@ -1,19 +1,25 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const hbs = require('hbs')
 
 //def paths for express
 const publicDirectoryPath = path.join(__dirname, '../public')
+const partialsPath = path.join(__dirname, '../templates/partials')
 const viewsPath = path.join(__dirname, '../templates/views');
 
 
 //setup handlebars engine and views location
 
-app.set('views engine', 'hbs'); //telling express/node to use handlebars for templates
+app.set('view engine', 'hbs'); //telling express/node to use handlebars for templates
 app.set('views', viewsPath)//telling express to get templates from templates/views
-app.get("", async(req, res) => {
+hbs.registerPartials(partialsPath);
+app.get("/about/:id", async(req, res) => {
+  const name = req.params.id;
   try{
-    res.render('index');
+    res.render('index', {
+      title: `${name}`
+    });
   } catch (error) {
     res.status(500).send('Page not found');
   }
@@ -23,6 +29,14 @@ app.get("/thamisucks", (req, res) => {
 });
 app.listen(3000, () => {
   console.log("Listening on port 3000");
+});
+app.get("/swag", async(req, res) => {
+
+  try{
+    res.render('swag');
+  } catch (error) {
+    res.status(500).send('Page not found');
+  }
 });
 
 //127.0.0.1:3000
