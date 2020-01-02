d9cf0dd76e875836f92c2d18cde41f8b5f2b82e9
cdugo <54993517+cdugo@users.noreply.github.com>
2020-01-02 11:31:13 -0500

    Update bundle.js

--- src/bundles/bundle.js
+++ src/bundles/bundle.js
@@ -1,15 +1,15 @@
-import {elements} from './elements';
+import { elements } from "./elements";
 
-elements.amiiboForm.addEventListener('keypress', function(event){
-    if (event.keyCode === 13){
-        event.preventDefault();
-        document.querySelector('.btn').click();
-    }
-})
+elements.amiiboForm.addEventListener("keypress", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.querySelector(".btn").click();
+  }
+});
 
-function search(){
-    elements.amiiboForm.addEventListener('submit', async function(e){
-        e.preventDefault();
-        const amiiboURL = `https://www.amiiboapi.com/api/amiibo/?character=${elements.amiiboName.value}`
-    })
+function search() {
+  elements.amiiboForm.addEventListener("submit", async function(e) {
+    e.preventDefault();
+    const amiiboURL = `https://www.amiiboapi.com/api/amiibo/?character=${elements.amiiboName.value}`;
+  });
 }
